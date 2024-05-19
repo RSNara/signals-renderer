936746e33a00fa5fdd5bbdff77e3e16dfb3901ab
Ramanpreet Nara <ramanpreetnara@gmail.com>
2024-05-19 09:43:26 -0700

    Make CounterApp the main one.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import { default as CounterApp } from './apps/CounterApp';
 import { default as TodoApp } from './apps/TodoApp';
 
 console.clear();
-Render(document.getElementById('app'), [TodoApp, {}]);
+Render(document.getElementById('app'), [CounterApp, {}]);
